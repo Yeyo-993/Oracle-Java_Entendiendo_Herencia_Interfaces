3c8edb7e44e5c370479f448fb0ec173e17dff04e
Sergio Reinoso <slrs993@gmail.com>
2023-03-13 21:35:51 -0500

    Polimorfismo 1ra fase

---
+++ bytebank-heredado/src/TestReferencias.java
@@ -0,0 +1,16 @@
+
+public class TestReferencias {
+	
+	public static void main(String[] args) {
+		
+		//Elemento mas generico puede ser adaptado al elemento mas especifico.
+		Funcionario funcionario = new Gerente();
+		funcionario.setNombre("Sergio");
+		
+		Gerente gerente = new Gerente();
+		gerente.setNombre("Jimena");
+		
+		funcionario.setSalario(2000);
+		gerente.setSalario(10000);
+	}
+}
