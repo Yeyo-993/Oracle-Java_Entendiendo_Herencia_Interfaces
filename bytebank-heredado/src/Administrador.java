9cd8cc697b03e0882f83db7ab7364ecc0b17fc9e
Sergio Reinoso <slrs993@gmail.com>
2023-03-16 18:59:07 -0500

    Practicando herencia e interfaces

--- bytebank-heredado/src/Administrador.java
+++ bytebank-heredado/src/Administrador.java
@@ -1,22 +1,25 @@
 
 public class Administrador extends Funcionario implements Autenticable {
 	
+	private AutenticacionUtil util;
+	
+	public Administrador() {
+		this.util = new AutenticacionUtil();
+	}
+	
 	@Override
 	public double getBonificacion() {
-		// TODO Auto-generated method stub
-		return 0;
+		return this.getSalario();
+	}
+	
+	@Override
+	public void setClave(String clave) {
+		this.util.setClave(clave);
 	}
 
 	@Override
 	public boolean iniciarSesion(String clave) {
-		// TODO Auto-generated method stub
-		return false;
-	}
-
-	@Override
-	public void setClave(String clave) {
-		// TODO Auto-generated method stub
-		
+		return this.util.iniciarSesion(clave);
 	}
 
 }
